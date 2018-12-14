d0d8d3fafbd7f7c0cf43824d3f8e8fb5b63e0185
Claire <claire.ristow@gmail.com>
2018-12-14 10:57:02 -0500

    parent and child page navigation added

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -24,16 +24,33 @@
                 }
             ?>
             
+            <?php 
+            $testArray = get_pages(array(
+                'child_of' => get_the_ID()
+            ));
             
-            <!--
-            <div class="page-links">
-                <h2 class="page-links__title"><a href="#">About Us</a></h2>
-                <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
-                </ul>
-            </div>
-              -->
+            if ($the_parent or $testArray) { ?>
+                <div class="page-links">
+                    <h2 class="page-links__title"><a href="<?php echo get_permalink($the_parent); ?>"><?php echo get_the_title($the_parent); ?></a></h2>
+                    <ul class="min-list">
+                        <?php
+
+                            if ($the_parent) {
+                                $findChildrenOf = $the_parent;
+                            } else {
+                                $findChildrenOf = get_the_ID();
+                            }
+
+                            wp_list_pages(array(
+                                'title_li' => NULL,
+                                'child_of' => $findChildrenOf,
+                                'sort_column' => 'menu_order'
+                            ));
+
+                        ?>
+                    </ul>
+                </div>
+            <?php } ?>
 
             <div class="generic-content">
                 <?php the_content(); ?>
