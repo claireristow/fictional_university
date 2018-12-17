48d575b75e770a08c38df4f11fa9e83d7352457b
Claire <claire.ristow@gmail.com>
2018-12-17 12:51:13 -0500

    custom events page added with custom event posts

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -6,8 +6,8 @@ function university_files() {
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
-
 add_action('wp_enqueue_scripts', 'university_files');
+
 
 function university_features() {
     register_nav_menu('headerMenuLocation', 'Header Menu Location');
@@ -15,5 +15,4 @@ function university_features() {
     register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
-
 add_action('after_setup_theme', 'university_features');
