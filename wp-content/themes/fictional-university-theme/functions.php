3d6017c167cb22066c97bd14ded2ebf000b15fe1
Claire <claire.ristow@gmail.com>
2018-12-18 16:32:25 -0500

    added html template for DRY code and Google Map to Campuses

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -30,7 +30,8 @@ function page_banner($args = NULL) {
 }
 
 function university_files() {
-    wp_enqueue_script('main_university_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCA0mOruzcb7OKgksvi8fbgx6erTmYcQsc', null, '1.0', true);
+    wp_enqueue_script('main_university_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
@@ -75,3 +76,10 @@ function university_adjust_queries($query) {
     }
 }
 add_action('pre_get_posts', 'university_adjust_queries');
+
+
+function universityMapKey($api) {
+    $api['key'] = 'AIzaSyCA0mOruzcb7OKgksvi8fbgx6erTmYcQsc';
+    return $api;
+}
+add_filter('acf/fields/google_map/api', 'universityMapKey');
