0f7d4a3c38c9ce6c00ad9c9edd6be636b78fdfa7
Claire <claire.ristow@gmail.com>
2018-12-19 12:55:09 -0500

    open and close search functionality with keyboard events

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -7,23 +7,47 @@ class Search {
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
         this.events();
+        this.isOverlayOpen = false;
     }
 
     // 2. events (clicks, keyboard events, etc.)
 
     events() {
+        // when search and close icons are clicked
         this.openButton.on('click', this.openOverlay.bind(this));
         this.closeButton.on('click', this.closeOverlay.bind(this));
+
+        // when 2 and esc are clicked on the keyboard... 
+        $(document).on('keydown', this.keyPressDispatcher.bind(this));
     }
 
     // 3. methods (function, action...)
 
+    keyPressDispatcher(e) {
+
+        // if "s" is clicked... 
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        };
+
+        // if "esc" is clicked... 
+        if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
+
     openOverlay() {
         this.searchOverlay.addClass('search-overlay--active');
+        $('body').addClass('body-no-scroll');
+        console.log("open fired");
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.removeClass('search-overlay--active');
+        $('body').removeClass('body-no-scroll');
+        console.log('close fired');
+        this.isOverlayOpen = false;
     }
 
 }
