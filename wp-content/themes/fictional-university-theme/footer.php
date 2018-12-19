817bcbc8060a44617611b9732e05b9696e98b577
Claire <claire.ristow@gmail.com>
2018-12-19 15:01:50 -0500

    search results working for keywords

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -81,7 +81,7 @@
         <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
       </div>
     </div>
-    <div class="continer">
+    <div class="container">
       <div id="search-overlay__results"></div>
     </div>
   </div>
--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -64,8 +64,13 @@ class Search {
 
     getResults() {
         console.log('getResults fired')
-        $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), function(posts) {
-            alert(posts[0].title.rendered);
+        $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+            this.resultsDiv.html(`
+                <h2 class="search-overlay__section-title">General Information</h2>
+                <ul class="link-list min-list">
+                    ${posts.map(post => `<li><a href="${post.link}">${post.title.rendered}</a></li>`).join('')}
+                </ul>
+            `);
         });
     }
 
